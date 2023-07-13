4211c1bf29a4537363fb6867ecec7a532207a447
Alina Custură <68774898+alinaai8@users.noreply.github.com>
2023-07-13 12:19:06 +0300

    Explosion Particle Effect.

--- Asteroids/Assets/Scripts/Asteroid.cs
+++ Asteroids/Assets/Scripts/Asteroid.cs
@@ -44,7 +44,9 @@ public class Asteroid : MonoBehaviour
                 CreateSplit();
             }
 
+            FindObjectOfType<GameManager>().AsteroidDestroyed(this);
             Destroy(this.gameObject);
+
         }
     }
 
--- Asteroids/Assets/Scripts/GameManager.cs
+++ Asteroids/Assets/Scripts/GameManager.cs
@@ -5,11 +5,20 @@ public class GameManager : MonoBehaviour
     public Player player;
     public int lives = 3;
     public float respawnTime = 3.0f;
-
     public float respawnInvulenarbilityTime = 3.0f;
+    public ParticleSystem explosion;
+    public int score = 0;
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        this.explosion.transform.position = asteroid.transform.position;
+        this.explosion.Play();
+    }
 
     public void PlayerDied()
     {
+        this.explosion.transform.position = this.player.transform.position;
+        this.explosion.Play();
+
         this.lives--;
 
         if(this.lives <= 0)
