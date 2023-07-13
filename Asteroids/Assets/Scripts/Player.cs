5decff94076b442fd77adf1a009ec7d858a62ec1
Alina Custură <68774898+alinaai8@users.noreply.github.com>
2023-07-13 12:05:04 +0300

    Dying and Respawning

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
+
     private void Update()
     {
         _thrusting = (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow));
@@ -50,5 +51,18 @@ public class Player : MonoBehaviour
     {
         Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
         bullet.Project(this.transform.up);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = 0.0f;
+
+            this.gameObject.SetActive(false);
+
+            FindObjectOfType<GameManager>().PlayerDied(); //Not the best way to do this but I have used it for simplicity
+        }
     }
 }
